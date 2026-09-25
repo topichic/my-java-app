e67e66e6d8f7163256c954caf94a19186e19b3ed
topichic <temochkachudin2008@gmail.com>
2026-09-25 11:28:43 +0300

    upd

--- src/main/java/Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java in Docker! ☕🐳");
-        System.out.println("Java version: " + System.getProperty("java.version"));
-        System.out.println("OS: " + System.getProperty("os.name"));
-    }
-}
